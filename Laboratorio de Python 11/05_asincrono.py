2f8dfc351a29da3c3575ca1f20a6ee59b9ea6e26
JIVANSD <100182555+JIVANSD@users.noreply.github.com>
2022-06-16 04:57:13 -0500

    Add files via upload

---
+++ Laboratorio de Python 11/05_asincrono.py
@@ -0,0 +1,44 @@
+from lib2to3.pytree import _Results
+import multiprocessing as mp
+from multiprocessing import pool
+import numpy as np
+import time
+
+def my_function(i, param1, param2, param3):
+    # calcula un polinomio
+    result = param1**2 + param2 + param3
+    time.sleep(2)
+    return (i,result)
+
+def get_result(result):
+    # se inscriben en la lista global
+    # (mal estilo de programación)
+    global results
+    results.append(result)
+
+#=====================
+#  Programa principal
+#=====================
+if __name__ == "__main__":
+
+    # Matriz de 10x3 números al azar
+    params = np.random.random((10,3))*100.0
+    results = []
+    ts = time.time()
+
+    # Un grupo de procesos (pool)
+    pool - mp.Pool(mp.cpu_count())
+
+    # loop de primera dimensión del arreglo
+    for i in range(0, params.shape[0]):
+        # Correr asincrónicamente my_function con argumentos args y cuando termine correr get_result
+        pool.apply_async(my_function, args = (i,params[i,0], params[i,1], params[i,2]), callback=get_result)
+
+    # Cerrar el grupo
+    pool.close()
+    # Esperar que termine el grupo
+    pool.join()
+
+    print("Tiempo en paralelo = ", time.time()-ts)
+    print(results)
+
---
+++ Laboratorio de Python 9/02_tareas_mpi.py
@@ -0,0 +1,36 @@
+from mpi4 import MPI
+
+#===========================
+#  Ojetos de comunicadores
+#===========================
+comm = MPI.COMM_WORLD
+
+#========================
+# Cuántos somos en total
+#========================
+size = comm.Get_size()
+
+#===========
+# Quien soy
+#===========
+rank = comm.Get_rank()
+
+#=============================
+# Si yo soy el cero hago esto
+#=============================
+if rank == 0:
+    print("Yo soy el proceso 0")
+
+#==================================
+# Si yo soy el uno hago esto otro
+#==================================
+elif rank == 1:
+    print("Yo soy el proceso 1")
+
+#=============================================
+# Si yo no soy ni el uno ni el dos hago esto
+#=============================================
+else:
+    print("Yo no soy ninguno de los dos primeros procesos")
+
+print("Reportándome, soy el proceso ", str(rank), " de ", str(size))
